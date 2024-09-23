b55bb1cc311321c6505d2e22e0d479b4b2aa79ea
William Deo Remulla <wremulla1@gmail.com>
2024-09-23 17:15:55 +0800

    true final

--- lab3.py
+++ lab3.py
@@ -1,7 +1,6 @@
 salary = int(input("Enter your salary: "))
-print(salary)
+
 loan = int(input("Enter loan amount: "))
-print(loan)
 
 if (salary >= int(30000) and loan < salary*10):
     print("You are eligible for the loan")
@@ -9,10 +8,11 @@ elif salary<int(30000) or loan>salary*10:
     print("You are not eligible for this loan")
 
 if salary<30000: 
-    print("Reason: Salary too low")
+    print("Reason: Salary too low"),exit()
     pass
 if loan>salary*10:
-    print("Reason: Loan too high")
+    print("Reason: Loan too high"),exit()
     pass
+
 month = int(input("Months to pay: "))
 print("Loan with interest:", (loan*1.10))
