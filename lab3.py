54f60068df637b6100ff1569aaf57a6d0bf31680
WilliamDeoRemulla <wremulla1@gmail.com>
2024-09-23 16:14:59 +0800

    Delete lab3.py

--- lab3.py
+++
@@ -1,6 +0,0 @@
-salary = input("Enter your salary: ")
-print(salary)
-loan = input("Enter loan amount: ")
-if [salary >= 30000] [loan < (salary*10)]:
-    print("You are eligible for the loan")
-else: 
